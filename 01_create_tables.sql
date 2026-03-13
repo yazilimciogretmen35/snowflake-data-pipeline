bdcf8b24564dd0358a7ce9e6b329f25b8616addb
metin <146376581+yazilimciogretmen35@users.noreply.github.com>
2026-03-13 17:52:29 +0300

    Create 01_create_tables.sql

---
+++ 01_create_tables.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TABLE ai_job_market(
+    job_id INT,
+    job_title STRING,
+    company_size STRING,
+    company_industry STRING,
+    country STRING,
+    remote_type STRING,
+    experience_level STRING,
+    years_experience INT,
+    education_level STRING,
+    skills_python BOOLEAN,
+    skills_sql BOOLEAN,
+    skills_ml BOOLEAN,
+    skills_deep_learning BOOLEAN,
+    skills_cloud BOOLEAN,
+    salary INT,
+    job_posting_month INT,
+    job_posting_year INT,
+    hiring_urgency STRING,
+    job_openings INT
+  
+    );
