26827c61e677dcf6877e1a026a00188b71bbe1e6
metin <146376581+yazilimciogretmen35@users.noreply.github.com>
2026-03-14 01:03:41 +0300

    Create 05_stream_task.sql

---
+++ 05_stream_task.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE STREAM ai_jobs_stream
+ON TABLE ai_job_market;
+
+INSERT INTO ai_job_market
+(10346,'AI Engineer','Startup','Education','Turkey','Hybrid','Mid',2,'Master',1,1,1,1,1,70000,2,2023,'High'1);
+
+CREATE OR REPLACE TASK ai_job_market_task
+Warehouse=compute_wh
+Shedule='5 Minute'
+AS
+  INSERT INTO ai_job_market_clean
+SELECT 
+    job_id,
+        job_title,
+        company_size,
+        company_industry,
+        country,
+        remote_type,
+        experience_level,
+        years_experience,
+        education_level,
+        skills_python,
+        skills_sql,
+        skills_ml,
+        skills_deep_learning,
+        skills_cloud,
+        salary,
+        job_posting_month,
+        job_posting_year,
+        hiring_urgency,
+        job_openings
+FROM ai_jobs_stream;
+
+
+ALTER TASK ai_job_market_task RESUME;
+
+
